3b594ccb229741ef5aa45e4615765c21c3848290
Edgar Parra <parra.ed@northeastern.edu>
2026-09-19 19:25:26 -0400

    Add: zigzag-conversion - submission-0

---
+++ Data Structures & Algorithms/zigzag-conversion/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        rows = [""] * numRows
+        row = 0
+        direction = 1
+        for c in s:
+            rows[row] += c            
+            if row == 0:
+                direction = 1
+            elif row == numRows - 1:
+                direction = -1
+            row += direction
+        return "".join(rows)   
